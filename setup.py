ead8a48e92297461533966584a7f42e92781303e
Stephen Tweedie <sct@redhat.com>
2023-02-03 16:35:25 +0000

    Bump to version 0.99.8

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="csvdiff3",
-    version="0.99.7",
+    version="0.99.8",
     author="Stephen Tweedie",
     author_email="sct@redhat.com",
     description="3-way CSV file merging utility",
